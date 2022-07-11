553276c1ff9cd93c6cdd66183cdbc31b5009f9c8
Sophie Toussaint <sophfrom76@hotmail.com>
2022-07-11 17:52:24 +0200

    add paramater + logout + controllers

---
+++ views/logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once '../controllers/logout-controller.php';
+?>
+
+<?php include '../elements/top.php' ?>
+
+
+<body class="d-flex flex-column min-vh-100">
+<?php include '../elements/header.php' ?>
+
+    <div class="container bienvenue d-flex align-items-center flex-column rounded my-5 p-5 border border-dark shadow">
+        <div class="col-lg-8 col-12 text-center fw-bold">
+            <p>Vous avez bien été déconnecté de votre compte.</p>
+        </div>
+    </div>
+
+    <?php include '../elements/footer.php' ?>
+
+</body>
+
+</html>
